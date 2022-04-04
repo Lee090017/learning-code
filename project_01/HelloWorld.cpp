7b8eda95bda772a2083947c14df6604b6cd44e69
Lee090017 <455938969@qq.com>
2022-04-04 19:43:51 +0800

    init

---
+++ project_01/HelloWorld.cpp
@@ -0,0 +1,15 @@
+/*
+ * @Author: your name
+ * @Date: 2022-04-04 16:18:35
+ * @LastEditTime: 2022-04-04 19:43:25
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \learningCode\project_01\helloworld.cpp
+ */
+#include<iostream>
+using namespace std;
+int main(){
+    cout << "Hello, world!" << endl;
+    cout << "实现Gitup上传" << endl;
+    return 0;
+}
